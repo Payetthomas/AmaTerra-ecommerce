564d828c26a125d10d89fc0d4af394eb6d5b8870
Thomas Payet <thomas@macbook-pro-de-thomas.home>
2025-05-05 18:05:17 +0400

    mise en place de la page catalogue

--- Frontend/src/components/Cards/Card/Card.tsx
+++ Frontend/src/components/Cards/Card/Card.tsx
@@ -1,28 +1,26 @@
 // src/components/Card/Card.tsx
 import React from "react";
-import styles from "./Card.module.scss"; // ✅ Import du CSS module
+import styles from "./Card.module.scss";
+import { Link } from "react-router-dom";
 
-type TCardProps = {
-  title: string;
-  image: string;
-  price: number;
-  description: string;
-  link?: string;
-};
+import { TProduct } from "../../../@types/cardTypes";
 
-export default function CardProduct(product: TCardProps) {
+export default function CardProduct({id,
+  title,
+  price,
+  description,
+  image,
+  }: TProduct) {
   return (
     <div className={styles.card}>
-      <img src={product.image} alt={product.title} className={styles.image} />
+      <img src={image} alt={title} className={styles.image} />
       <div className={styles.content}>
-        <h2 className={styles.title}>{product.title}</h2>
-        <p className={styles.description}>{product.description}</p>
-        <p className={styles.price}>{product.price.toFixed(2)} €</p>
-        {product.link && (
-          <a href={product.link} className={styles.button}>
+        <h2 className={styles.title}>{title}</h2>
+        <p className={styles.description}>{description}</p>
+        <p className={styles.price}>{price.toFixed(2)} €</p>
+          <Link to={`/produits/${id}`} className={styles.button}>
             Découvrir
-          </a>
-        )}
+          </Link>
       </div>
     </div>
   );
---
+++ Frontend/src/pages/Catalogues/Catalogues.tsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+import styles from "./Catalogues.module.scss"; 
+import CardProduct from "../../components/Cards/Card/Card";
+import { TProduct } from "../../@types/cardTypes";
+
+const Catalogues = () => {
+
+
+
+    return (
+        <main className="container-product">
+             <section className={styles.catalogue}>
+                <h1 className={styles.title}>Notre Catalogue</h1>
+                <div className={styles.grid}>
+                    {data.map((product: TProduct) => (
+                    <CardProduct
+                        key={product.id}
+                        {...product}
+                    />
+                    ))}
+                </div>
+            </section>
+        </main>
+
+    );
+}; 
+
+export default Catalogues;   
